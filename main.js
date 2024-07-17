a54ca34fb55d0975a28ae7d30f9fe448b042f583
femiolulana <oniseikeji@gmail.com>
2024-07-17 03:36:24 -0400

    a commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const APILINK ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8322429dd8f3cbb9780c43e74e8f2b28&page=1"
+const IMG_PATH ="https://image.tmdb.org/t/p/w1280"
+const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=8322429dd8f3cbb9780c43e74e8f2b28&query="
+
+const main = document.getElementById('section')
+const main = document.getElementById('section')
+const main = document.getElementById('section')
