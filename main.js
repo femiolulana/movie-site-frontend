4c20f38b2955d5646b2ceeeb0a3288b2abd03fee
femiolulana <oniseikeji@gmail.com>
2024-07-20 05:14:34 -0400

    a commit

--- main.js
+++ main.js
@@ -2,26 +2,57 @@ const APILINK ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.d
 const IMG_PATH ="https://image.tmdb.org/t/p/w1280"
 const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=8322429dd8f3cbb9780c43e74e8f2b28&query="
 
-const main = document.getElementById('section')
-const form = document.getElementById('form')
-const search = document.getElementById('query')
 
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
 
 returnMovies(APILINK)
+function returnMovies(url){
+  fetch(url).then(res => res.json())
+  .then(function(data){
+  console.log(data.results);
+  data.results.forEach(element => {
+      const div_card = document.createElement('div');
+      div_card.setAttribute('class', 'card');
+      
+      const div_row = document.createElement('div');
+      div_row.setAttribute('class', 'row');
+      
+      const div_column = document.createElement('div');
+      div_column.setAttribute('class', 'column');
+      
+      const image = document.createElement('img');
+      image.setAttribute('class', 'thumbnail');
+      image.setAttribute('id', 'image');
+      
+      const title = document.createElement('h3');
+      title.setAttribute('id', 'title');
+      
+      const center = document.createElement('center');
 
-function returnMovies(url) {
-    fetch(url)
-        .then(res => res.json())
-        .then(function(data) { 
-            console.log(data.results);
-            data.results.forEach(element => {
-                const div_card = document.createElement("div");
-                const div_row = document.createElement("div");
-                const div_column = document.createElement("div");
-                const image = document.createElement("img");
-                const title = document.createElement("h3");
-                const center = document.createElement("center ");
-                // Additional logic can be added here
-            });
-        });
+      title.innerHTML = `${element.title}`;
+      image.src = IMG_PATH + element.poster_path;
+
+      center.appendChild(image);
+      div_card.appendChild(center);
+      div_card.appendChild(title);
+      div_column.appendChild(div_card);
+      div_row.appendChild(div_column);
+
+      main.appendChild(div_row);
+  });
+});
 }
+
+form.addEventListener("submit", (e) => {
+e.preventDefault();
+main.innerHTML = '';
+
+const searchItem = search.value;
+
+if (searchItem) {
+  returnMovies(SEARCHAPI + searchItem);
+    search.value = "";
+}
+});
