036005a2dccbb0a8a907fa168e4b476ad0a79b05
femiolulana <oniseikeji@gmail.com>
2024-07-20 03:10:14 -0400

    a commit

--- main.js
+++ main.js
@@ -3,5 +3,25 @@ const IMG_PATH ="https://image.tmdb.org/t/p/w1280"
 const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=8322429dd8f3cbb9780c43e74e8f2b28&query="
 
 const main = document.getElementById('section')
-const main = document.getElementById('section')
-const main = document.getElementById('section')
+const form = document.getElementById('form')
+const search = document.getElementById('query')
+
+
+returnMovies(APILINK)
+
+function returnMovies(url) {
+    fetch(url)
+        .then(res => res.json())
+        .then(function(data) { 
+            console.log(data.results);
+            data.results.forEach(element => {
+                const div_card = document.createElement("div");
+                const div_row = document.createElement("div");
+                const div_column = document.createElement("div");
+                const image = document.createElement("img");
+                const title = document.createElement("h3");
+                const center = document.createElement("center ");
+                // Additional logic can be added here
+            });
+        });
+}
